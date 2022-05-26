8b923ce5c6e9c3e9cd9c94dc7f8b773be48b8e2d
Michael Wright <michael@cronai.ai>
2022-05-26 12:18:57 +0100

    day11

--- day11.py
+++ day11.py
@@ -2,12 +2,12 @@ from common import day
 import numpy
 
 def part1(lines) -> int: 
-    s = seating(lines)
+    s = seating(lines, True)
     return s.countStability()
 
 def part2(lines) -> int:
-    
-    return 0
+    s = seating(lines, False)
+    return s.countStability()
 
 class seating:
 
@@ -17,25 +17,25 @@ class seating:
     FLOOR_CHAR = "."
 
 
-    def __init__(self, lines):
+    def __init__(self, lines, isPart2):
         self.height = len(lines)
         self.width = len(lines[0])
         self.xRange = range(self.width)
         self.yRange = range(self.height)
         self.grid = numpy.full((self.height, self.width), self.FLOOR_CHAR)
+        self.isPart2 = isPart2
+        self.threshold = 5 if isPart2 else 4
 
         for x in self.xRange:
             for y in self.yRange:
-                self.grid[x][y] = lines[x][y]
-
-        self.print()
+                self.grid[y][x] = lines[y][x]
 
     def countStability(self) -> int:
         prevCount = -1
         for i in range(self.TIME_OUT):
             self.performCycle()
-            self.print()
-            newCount = self.countChar(self.OCCUPIED_CHAR)            
+            newCount = self.countChar(self.OCCUPIED_CHAR)
+            #self.print()
             if (prevCount == newCount):
                 return newCount
             else:
@@ -47,49 +47,66 @@ class seating:
         counter = 0
         for x in self.xRange:
             for y in self.yRange:
-                if char == self.grid[x][y]:
+                if char == self.grid[y][x]:
                     counter += 1
         return counter
 
     def print(self):
         for y in self.yRange:
-            print(self.grid[0:self.width][y])
+            printStr = ""
+            for x in self.xRange:
+                printStr += self.grid[y][x]
+            print(printStr)
         print(" ")
 
 
     def performCycle(self):
-        newGrid = numpy.full((self.width, self.height), self.FLOOR_CHAR)
+        newGrid = numpy.full((self.height, self.width), self.FLOOR_CHAR)
 
         for x in self.xRange:
             for y in self.yRange:
-                char = self.grid[x][y]
+                char = self.grid[y][x]
                 if char == self.FLOOR_CHAR:
                     continue
 
                 neighbours = self.countNeighbours(x, y)
                 if char == self.EMPTY_CHAR and neighbours == 0:
-                    newGrid[x][y] = self.OCCUPIED_CHAR
-                elif char == self.OCCUPIED_CHAR and neighbours >= 4:
-                    newGrid[x][y] = self.EMPTY_CHAR
+                    newGrid[y][x] = self.OCCUPIED_CHAR
+                elif char == self.OCCUPIED_CHAR and neighbours >= self.threshold:
+                    newGrid[y][x] = self.EMPTY_CHAR
                 else:
-                    newGrid[x][y] = self.grid[x][y]
+                    newGrid[y][x] = self.grid[y][x]
 
         self.grid = newGrid                
 
     def countNeighbours(self, x, y) -> int:
-        xStart = x - 1 if x > 0 else 0
-        xEnd = x + 1 if x < self.width - 1 else self.width - 1
-        yStart = y - 1 if y > 0 else 0
-        yEnd = y + 1 if y < self.width - 1 else self.width - 1
         counter = 0
 
-        for i in range(xStart, xEnd):
-            for j in range(yStart, yEnd):
-                if i == x and j == y:
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == 0 and j == 0:
                     continue
-                elif self.grid[i][j] == self.OCCUPIED_CHAR:
+                elif self.getSeenSeat(x, y, i, j) == self.OCCUPIED_CHAR:
                     counter += 1
 
         return counter
+
+    def getSeenSeat(self, x, y, xDir, yDir) -> str:
+        i = 1
+        while (True):
+            xCheck = x + (xDir * i)
+            yCheck = y + (yDir * i)
+            if (xCheck < 0 or yCheck < 0 or xCheck == self.width or yCheck == self.height):
+                return self.EMPTY_CHAR
+            seat = self.grid[yCheck, xCheck]
+
+            if (not self.isPart2):
+                return seat
+            elif (seat != self.FLOOR_CHAR):
+                return seat
+            else:
+                i += 1
+
+
 
 day.run(part1, part2, 11)
