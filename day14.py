3f89ed494fef3381df17978dcc0c5fdfc01eca94
Mike Wright <thespoonftw@gmail.com>
2022-05-28 10:37:38 +0100

    day 14b

--- day14.py
+++ day14.py
@@ -1,9 +1,15 @@
+from dataclasses import replace
 from common import day
 
 def part1(lines) -> int:
+    return solve(lines, False)
+    
 
-    memory = [0 for i in range(99999)]
+def part2(lines) -> int:
+    return solve(lines, True)
 
+def solve(lines, isPartTwo) -> int:
+    memory = dict()
     currentMask = ""
 
     for line in lines:
@@ -13,21 +19,51 @@ def part1(lines) -> int:
         else:
             location = int(split[0].split("[")[1][:-1])
             value = int(split[1])
-            binary = format(value, '036b')
-            binarySum = 0
-
-            for i, char in enumerate(currentMask):
-                power = 35 - i
-                digit = int(char) if char != 'X' else int(binary[i])
-                binarySum += digit * pow(2, power)
-
-            memory[location] = binarySum
-
-    return sum(memory)
-
-def part2(lines) -> int:
-    return 0
+            if (isPartTwo):
+                for newLocation in applyMaskToLocations(currentMask, location):
+                    memory[newLocation] = value
+            else:
+                memory[location] = applyMaskToValue(currentMask, value)
             
 
+    return sum(memory.values())
+
+def applyMaskToValue(mask, value) -> int:
+    binary = format(value, '036b')
+    binarySum = 0
+    for i, char in enumerate(mask):
+        power = 35 - i
+        digit = int(char) if char != 'X' else int(binary[i])
+        binarySum += digit * pow(2, power)
+
+    return binarySum
+
+def applyMaskToLocations(mask, location) -> list:
+    returner = list()
+    binary = format(location, '036b')
+    recursiveFindValues(mask, binary, returner, 0)
+    return returner
+
+def recursiveFindValues(mask, binary, returner, index):
+    if (index == len(mask)):
+        returner.append(int(binary, 2))
+        return
+    
+    char = mask[index]
+    if (char == "X"):
+        newBinary1 = replaceChar(binary, index, "1")
+        newBinary2 = replaceChar(binary, index, "0") 
+        recursiveFindValues(mask, newBinary1, returner, index + 1)
+        recursiveFindValues(mask, newBinary2, returner, index + 1)
+
+    elif (char == "1"):
+        newBinary = replaceChar(binary, index, "1")
+        recursiveFindValues(mask, newBinary, returner, index + 1)
+
+    elif (char == "0"):
+        recursiveFindValues(mask, binary, returner, index + 1)
+
+def replaceChar(string, index, char):
+    return string[0:index] + char + string[index+1:]
 
 day.run(part1, part2, 14)
