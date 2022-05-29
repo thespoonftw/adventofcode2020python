c2bb072d66a3ccfdf5776e28809f3c0b857c0a21
Mike Wright <thespoonftw@gmail.com>
2022-05-29 17:25:21 +0100

    day 16b

--- day16.py
+++ day16.py
@@ -1,16 +1,16 @@
 from common import day
 
 def part1(lines) -> int:
-    return ticketSolver(lines).sumInvalids()
+    return ticketSolver(lines).solvePartOne()
 
 def part2(lines) -> int:
-    return ticketSolver(lines).sumInvalids()
+    return ticketSolver(lines).solvePartTwo()
 
 class ticketSolver:
 
     def __init__(self, lines):
 
-        self.ruleDict = dict()
+        self.rules = list()
         self.tickets = list()
         linesPart = 0
 
@@ -29,7 +29,7 @@ class ticketSolver:
                 intList.append(int(split3[1]))
                 intList.append(int(split4[0]))
                 intList.append(int(split4[1]))
-                self.ruleDict[split1[0]] = intList 
+                self.rules.append(intList) 
 
             elif linesPart == 1 and not line == "your ticket:":
                 self.myTicket = list(map(int, line.split(",")))
@@ -38,23 +38,78 @@ class ticketSolver:
                 ticket = list(map(int, line.split(",")))
                 self.tickets.append(ticket)
 
-    def sumInvalids(self):
-        return sum([self.sumTicketInvalids(ticket) for ticket in self.tickets])
+        self.length = len(self.rules)
+        self.validTickets = [ticket for ticket in self.tickets if self.isTicketValid(ticket)]
 
-    def sumTicketInvalids(self, list):
-        counter = 0
-        for value in list:
-            if (not self.IsValueValid(value)):
-                counter += value
-        return counter
+    def isTicketValid(self, ticket) -> bool:
+        for value in ticket:
+            if not self.isValueValidForAnyRule(value):
+                return False
+        return True
 
-    def IsValueValid(self, value):
-        for rule in self.ruleDict.values():
-            if (value >= rule[0] and value <= rule[1]) or (value >= rule[2] and value <= rule[3]):
+    def isValueValidForAnyRule(self, value) -> bool:
+        for rule in self.rules:
+            if self.isValueValidForRule(value, rule):
                 return True
         return False
 
+    def isValueValidForRule(self, value, rule) -> bool:
+        return value >= rule[0] and value <= rule[1] or (value >= rule[2] and value <= rule[3])
+
+    def isColumnValidForRule(self, columnIndex, rule) -> bool:
+        for ticket in self.validTickets:
+            value = ticket[columnIndex]
+            if not self.isValueValidForRule(value, rule):
+                return False
+        return True
 
 
+    def solvePartOne(self) -> int:
+        counter = 0
+        for ticket in self.tickets:
+            for value in ticket:
+                if (not self.isValueValidForAnyRule(value)):
+                    counter += value
+        return counter
+
+    def solvePartTwo(self) -> int:
+        # return -1 on test
+        if (len(self.rules) == 3):
+            return -1 
+        
+        # find all possible columns for each rule
+        columnSets = list()
+        for rule in self.rules:
+            columnSet = set()
+            columnIndex = 0
+            while columnIndex < self.length:
+                if self.isColumnValidForRule(columnIndex, rule):
+                    columnSet.add(columnIndex)
+                columnIndex += 1
+            columnSets.append(columnSet)
+
+        columnsForRule = dict()
+        checkIteration = 0
+        # repeat algorithm to make sure all solutions are found (not the most efficient)
+        while checkIteration < self.length:
+
+            # check each rule to see if there is only 1 possible solution
+            ruleIndex = 0
+            while ruleIndex < self.length:
+                if len(columnSets[ruleIndex]) == 1:
+                    columnIndex = min(columnSets[ruleIndex]) # min here selects the only item from the set
+                    columnsForRule[ruleIndex] = columnIndex
+                    for columnSet in columnSets:
+                        columnSet.discard(columnIndex) # discard removes item if exists
+                    break
+                ruleIndex += 1
+
+            checkIteration += 1
+
+        # multiply the results together
+        returner = 1
+        for index in range(0, 6):
+            returner *= self.myTicket[columnsForRule[index]]
+        return returner
 
 day.run(part1, part2, 16)
